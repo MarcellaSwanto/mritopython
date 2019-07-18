8dea64a4ba4801f2e0f59904b2ee7f344321235a
MarcellaSwanto <32901873+MarcellaSwanto@users.noreply.github.com>
2019-07-18 10:55:14 +0100

    Delete Plugin_ResetK.py

--- Plugin_ResetK.py
+++
@@ -1,7 +0,0 @@
-# from DoExtPlugin import VVar
-
-#important data type conversion, has to be done!!!!
-def Plugin_ResetK():
-    VVar["Kz"] = double(0)
-    VVar["Ky"] = double(0)
-    VVar["Kx"] = double(0)
